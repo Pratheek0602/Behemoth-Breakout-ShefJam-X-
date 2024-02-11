de7f6eb3003278a66a122ba04180f225104ff7a3
Pratheek Vasu <pvasu1@sheffield.ac.uk>
2024-02-11 08:22:56 +0000

    Completed UI

--- Behemoth Breakout/Assets/CollisionDetection.cs
+++ Behemoth Breakout/Assets/CollisionDetection.cs
@@ -11,6 +11,7 @@ public class CollisionDetection : MonoBehaviour
         if(other.tag == "Enemy" && wc.isAttacking){
             Debug.Log(other.name);
             other.GetComponent<Animator>().SetTrigger("Hit");
+            Debug.Log("HITTINGGGGGGGGGGGGGGGGGGGGGGGGGG");
 
             Instantiate(HitParticle, new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z),
             other.transform.rotation);
